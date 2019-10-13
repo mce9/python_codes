b77403948b0ef2a059da2cda7f98b8e2940e5317
David W <dwing@noncosi.com>
2019-10-13 16:30:20 -0700

    Add reverseCipher.py

---
+++ reverseCipher.py
@@ -0,0 +1,10 @@
+message = 'Three can keep a secret, if two of them are dead.'
+translated = ''
+
+i = len(message) - 1
+while i >= 0:
+	translated = translated + message[i]
+	print('i is', i, ', message[i] is', message[i], ', translated is', translated)
+	i = i -1 
+
+print(translated)
